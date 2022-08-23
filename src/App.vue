cfc4952668f90a0f485efeb255c8f455981c790e
simone ricciardo <you@example.com>
2022-08-23 17:03:11 +0200

    concluso

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="container-hero">
+
+      <MyJumbo/>
+  
+      <MyTitle/>
+   
+   </div>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MyTitle from './components/MyTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTitle
   }
 }
 </script>
@@ -24,5 +34,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container-hero{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("/img/Banque images_featured.webp");
+  height: 50vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
 }
 </style>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <h1>La mia prima app con Vue CLI!</h1>
+    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+    h1{
+        color: rgb(255, 255, 255);
+    }
+
+</style>
